4ecd033ce2d134a1d8f6fabe4ab645b9c699883c
ph5500 <philfives@gmail.com>
2020-05-08 12:06:13 -0600

    finished routes from projectRouter, working on actionRouter

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -21,6 +21,52 @@ router.get("/:id", (req, res) => {
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({ errorMessage: "THere was an error retrieving the data." });
+            res.status(500).json({ errorMessage: "There was an error retrieving the data." });
         });
 });
+
+router.get("/:id", (req, res) => {
+    Actions.get(req.params.id)
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ errorMessage: "There was an error retrieving the data." });
+
+        });
+});
+
+router.post("/", (req, res) => {
+    if (!req.body.description || !req.body.project_id || !req.body.notes) {
+        res.status(404).json({ errorMessage: "Please provide a description, notes, and project id." });
+    } else {
+        Actions.insert(req.body)
+            .then(action => {
+                res.status(201).json(action);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ errorMessage: "The data could not be saved." });
+            });
+    }
+});
+
+router.put("/:id", (req, res) => {
+    Actions.update(req.params.id, req.body)
+        .then(action => {
+            if (action) {
+                res.status(200).json(action);
+            } else {
+                res.status(404).json({
+                    errorMessage: "The action with the specified ID does not exist."
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(404).json({
+                errorMessage: "The action with the specified ID does not exist."
+            });
+        })
+})
